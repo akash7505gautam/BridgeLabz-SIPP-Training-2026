06cadc1402be43226e87a3f0a97443d305756b7f
ambar gautam <akashgautam00234@gmail.com>
2026-06-09 10:40:51 +0530

    [Akash] Add: Day 1 updated

---
+++ core-java-practice/gcr-codebase/programming-elements/practice/kilometertomile.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class kilometertomile {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int km=sc.nextInt();
+        System.out.println(km*0.621371);
+    }
+    
+}
---
+++ core-java-practice/gcr-codebase/programming-elements/practice/welcome_to_bridge_labz.java
@@ -0,0 +1,5 @@
+public class welcome_to_bridge_labz {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Bridgelabz!");
+    }
+}
