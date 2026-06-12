057717f4be412651c6ae18b239f4efeadecbfdc8
ambar gautam <akashgautam00234@gmail.com>
2026-06-12 23:01:34 +0530

    [Akash] Added: cotrol flow  Level3

---
+++ core-java-practice/gcr-codebase/control-flow/level 3/LeapYearUsingSingleIfCondition.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class LeapYearUsingSingleIfCondition {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter year: ");
+        int year = sc.nextInt();
+        if (year >= 1582 &&
+            ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))) {
+            System.out.println(year + " is a Leap Year");
+        } else {
+            System.out.println(year + " is not a Leap Year");
+        }
+    }
+}
