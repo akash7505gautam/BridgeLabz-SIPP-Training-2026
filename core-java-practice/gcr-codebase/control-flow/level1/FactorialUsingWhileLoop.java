87398dca73c66849290cee6de2f1e4a30d4ce0f8
ambar gautam <akashgautam00234@gmail.com>
2026-06-12 22:57:49 +0530

    [Akash] Added: Control Flow Level 1

---
+++ core-java-practice/gcr-codebase/control-flow/level1/FactorialUsingWhileLoop.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class FactorialUsingWhileLoop {
+        public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if (n > 0) {
+            long fact = 1;
+            int i = 1;
+            while (i <= n) {
+                fact *= i;
+                i++;
+            }
+            System.out.println("Factorial = " + fact);
+        } else {
+            System.out.println("Not a natural number");
+        }
+    }
+}
