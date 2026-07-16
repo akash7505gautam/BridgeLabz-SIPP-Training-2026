ec6680badd754f59264637fcdeb4d3e5d7de9d97
ambar gautam <akashgautam00234@gmail.com>
2026-07-16 10:11:02 +0530

    [Akash] Added Hashing solutions

---
+++ Practice/Hashing/DuplicateSessionTokenDetection.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class DuplicateSessionTokenDetection {
+public boolean hasDuplicateToken(String[] tokens) {
+Set<String> seen = new HashSet<>();
+
+for (String token : tokens) {
+if (!seen.add(token)) {
+return true;
+}
+}
+return false;
+}
+}
